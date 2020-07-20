73973828628764ac5a855515db309394f2568e28
Alex Page <alexanderkpage@gmail.com>
2020-07-20 18:24:48 -0400

    chmod

--- run.sh
+++ run.sh
@@ -4,6 +4,7 @@ bzt = opt/rh/rh-python36/root/usr/bin/bzt
 
 for d in */ ; do
         echo "Looping through $d"
+		chmod 755 -R $d
         for filename in $d/*; do
                 if [[ $filename =~ \.yml$ ]]; then
                         echo "Executing ${filename/\//}"
