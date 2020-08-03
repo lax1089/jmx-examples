05fa8dbe54b50d36a80d0922022ff0759eaad5e7
Alex Page <alexanderkpage@gmail.com>
2020-08-03 16:18:56 -0400

    script updates to only run perf when yaml exists, and run all others as func

--- run.sh
+++ run.sh
@@ -6,13 +6,12 @@ for d in */ ; do
         echo "Looping through $d"
 		chmod 755 -R $d
         for filename in $d/*; do
-                if [[ $filename =~ \.yml$ ]]; then
-						mod_file_str=`echo ${filename##*/}`
-						mod_file_str=`echo ${mod_file_str%%.*}`
-                        echo "Executing ${filename/\//} as a functional test with name '${mod_file_str} Functional Test'"
-                        ${bzt} ${filename/\//} -cloud -func -o modules.blazemeter.test="${mod_file_str} Functional Test" -o modules.blazemeter.report-name="${mod_file_str} Functional Report"
-						echo "Executing ${filename/\//} as a performance test with name '${mod_file_str} Load Test'"
-                        ${bzt} ${filename/\//} -cloud -detach -o modules.blazemeter.test="${mod_file_str} Load Test" -o modules.blazemeter.report-name="${mod_file_str} Load Test Report"
+				if [[ $filename =~ \.jmx$ ]]; then
+						echo "Executing ${filename/\//} as a functional test"
+                        ${bzt} ${filename/\//} -cloud -func
+				elif [[ $filename =~ \.yml$ ]]; then
+						echo "Executing ${filename/\//} as a performance test"
+                        ${bzt} ${filename/\//} -cloud -detach
                 fi
         done
 done
