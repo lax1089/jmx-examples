9997a40b36f2a8f414604a447719bb5e7134b027
Alex Page <alexanderkpage@gmail.com>
2020-08-03 16:58:36 -0400

    toggle assertions

--- run.sh
+++ run.sh
@@ -1,14 +1,21 @@
 #!/bin/bash
 rm -rf 20*
-bzt=/opt/rh/rh-python36/root/usr/bin/bzt
+linux_bzt_loc=/opt/rh/rh-python36/root/usr/bin/bzt
+if [[ -f "$linux_bzt_loc" ]]; then
+	bzt=$linux_bzt_loc
+else
+	bzt="bzt"
+fi
 
 for d in */ ; do
         echo "Looping through $d"
 		chmod 755 -R $d
         for filename in $d/*; do
+				mod_file_str=`echo ${filename##*/}`
+				mod_file_str=`echo ${mod_file_str%%.*}`
 				if [[ $filename =~ \.jmx$ ]]; then
-						echo "Executing ${filename/\//} as a functional test"
-                        ${bzt} ${filename/\//} -cloud -func
+						echo "Executing ${filename/\//} as a functional test with name '${mod_file_str} Functional Test'"
+                        ${bzt} ${filename/\//} -cloud -func -detach -o modules.blazemeter.test="${mod_file_str} Functional Test" -o modules.blazemeter.report-name="${mod_file_str} Functional Report"
 				elif [[ $filename =~ \.yml$ ]]; then
 						echo "Executing ${filename/\//} as a performance test"
                         ${bzt} ${filename/\//} -cloud -detach
